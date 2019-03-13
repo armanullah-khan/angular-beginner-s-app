99168096300874ccd5ea117b7f52e6a2bfe24108
Your Name <you@example.com>
2019-03-13 00:12:26 -0700

    adding nav template

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  
+  appTitle: string = 'Angular Beginner App';
 
   constructor() { }
 
